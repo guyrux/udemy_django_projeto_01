42def08efdf20c443a4e6b0fb21357e954fa0cec
Gustavo Suto <guyrux@gmail.com>
2020-09-20 15:22:21 -0300

    [feat] Voltando a mexer nesse projeto/curso.

--- projeto01_udemy/base/views.py
+++ projeto01_udemy/base/views.py
@@ -1,3 +1,34 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
+
+# def home(request):
+#     return HttpResponse("Olá, Django! Finalmente!")
+
+def index(request):
+    produtos = Produto.objects.all()
+
+    context = {
+        "curso": "Programação web com framework django. \o/",
+        "texto2": "Ainda tentando ver se carrega em seguida.",
+        "produtos": produtos
+    }
+
+    return render(request, "index.html", context)
+
+
+def contato(request):
+    return render(request, "contato.html")
+
+
+def produto(request, pk):
+    print(f"pk: {pk}")
+    produto_selecionado = Produto.objects.get(id=pk)
+
+    context = {
+        "produto": produto_selecionado
+    }
+
+    return render(request, 'produto.html', context)
--- projeto01_udemy/settings.py
+++ projeto01_udemy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a09zfw0pbfkgiew5d&rj%n(*dnlibd@(6epd1vgbdgnmki219n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projeto01_udemy.base',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'projeto01_udemy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -117,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'     # Usar em ambiente de desenvolvimento
+STATIC_ROOT = '/static/'    # Usar em ambiente de produção
